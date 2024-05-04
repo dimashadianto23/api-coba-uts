7c12b873a0720141c3ccef82566868115caf34ec
dimashadi <dimashadianto460@gmail.com>
2024-05-04 13:34:44 +0700

    [FEATURE] Update function login in authentication-controller for login attempts limit

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -1,6 +1,10 @@
 const { errorResponder, errorTypes } = require('../../../core/errors');
 const authenticationServices = require('./authentication-service');
 
+const loginAttempts = {}; // Object to store login attempts
+const maxAttempts = 5; // Max login attempts allowed
+const attemptWindow = 30 * 60 * 1000; // 30 minutes in milliseconds
+
 /**
  * Handle login request
  * @param {object} request - Express request object
@@ -12,6 +16,24 @@ async function login(request, response, next) {
   const { email, password } = request.body;
 
   try {
+    // Check if login attempts exceeded
+    if (loginAttempts[email] && loginAttempts[email].attempts >= maxAttempts) {
+      // Check if attempt window has elapsed
+      if (Date.now() - loginAttempts[email].lastAttempt < attemptWindow) {
+        const remainingTime = Math.ceil(
+          (attemptWindow - (Date.now() - loginAttempts[email].lastAttempt)) /
+            60000
+        ); // Remaining time in minutes
+        throw errorResponder(
+          errorTypes.FORBIDDEN,
+          `Too many failed login attempts. Please try again in ${remainingTime} minutes.`
+        );
+      } else {
+        // Clear attempts if attempt window elapsed
+        delete loginAttempts[email];
+      }
+    }
+
     // Check login credentials
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
@@ -19,11 +41,25 @@ async function login(request, response, next) {
     );
 
     if (!loginSuccess) {
+      // Update login attempts
+      if (!loginAttempts[email]) {
+        loginAttempts[email] = {
+          attempts: 1,
+          lastAttempt: Date.now(),
+        };
+      } else {
+        loginAttempts[email].attempts++;
+        loginAttempts[email].lastAttempt = Date.now();
+      }
+
       throw errorResponder(
         errorTypes.INVALID_CREDENTIALS,
         'Wrong email or password'
       );
     }
+
+    // Clear login attempts on successful login
+    delete loginAttempts[email];
 
     return response.status(200).json(loginSuccess);
   } catch (error) {
